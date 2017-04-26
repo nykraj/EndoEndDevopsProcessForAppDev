fd0067901d73131c1d82cc379b79f9e3ec756002
Manupriya Gupta <manupriyagupta@manu.in.ibm.com>
2017-04-26 12:14:09 +0530

    New code added

---
+++ test/unit/frontend/dummy-test.js
@@ -0,0 +1,16 @@
+describe('my test suite', function() {
+
+  beforeEach(function() {
+
+  });
+
+  afterEach(function() {
+
+  });
+
+  it('karma test case', function(done) {
+    expect(true).to.be.equal(true);
+    done();
+  });
+
+});
